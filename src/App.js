88af51fbc38efd06319970d4a069b0796ad83cb9
Garnik Hovsepyan <garoh2291@gmail.com>
2022-06-09 14:24:06 +0400

    sdks

--- src/App.js
+++ src/App.js
@@ -1,31 +1,7 @@
-import { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { addNewTodo, fetchTodos } from "./store/todoSlice";
 import "./App.css";
-import { InputField } from "./compoents/InputField";
-import TodoList from "./compoents/TodoList";
 
 function App() {
-  const [text, setText] = useState("");
-  const { status, error } = useSelector((state) => state.todos);
-  const dispatch = useDispatch();
-  const addTask = () => {
-    dispatch(addNewTodo(text));
-    setText("");
-  };
-
-  useEffect(() => {
-    dispatch(fetchTodos());
-  }, [dispatch]);
-
-  return (
-    <div className="App">
-      <InputField text={text} setText={setText} handleSubmit={addTask} />
-      {status === "loading" && <h2>Loading ...</h2>}
-      {error && <h2>An error occured : {error}</h2>}
-      <TodoList />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
--- src/compoents/InputField.jsx
+++
@@ -1,8 +0,0 @@
-export const InputField = ({ text, setText, handleSubmit }) => {
-  return (
-    <label>
-      <input value={text} onChange={(e) => setText(e.target.value)} />
-      <button onClick={handleSubmit}>Add Todo</button>
-    </label>
-  );
-};
